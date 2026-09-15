d749edfc6de93343e5b89a959889ceaf52822208
Joona <joona.louhisalmi@gmail.com>
2026-09-15 20:17:16 +0300

    SCRUM-44 add authentication and login backend structure

---
+++ Backend/models/userModel.js
@@ -0,0 +1,16 @@
+import pool from '../db.js';
+
+async function findUserByEmail(email) {
+  const result = await pool.query(
+    `
+      SELECT id, email, username, password_hash
+      FROM users
+      WHERE email = $1
+    `,
+    [email]
+  );
+
+  return result.rows[0] || null;
+}
+
+export { findUserByEmail };
