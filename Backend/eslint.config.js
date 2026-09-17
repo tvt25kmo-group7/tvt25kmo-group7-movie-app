cee340c28be3a8ef052970ae5b46069015dce0f1
Emil <Koivunen.emil@gmail.com>
2026-09-17 10:14:15 +0300

    fix: update user routes and lint setup

---
+++ Backend/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: 'off',
+      eqeqeq: ['error', 'always'],
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrors: 'none',
+        },
+      ],
+    },
+  },
+];
