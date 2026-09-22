a9cb1b9aba4d85405447af27d24394ac57dd09f3
MariannaTaavitsainen <mariannataavitsainen@gmail.com>
2026-09-22 17:39:09 +0300

    Scrum 40 (#58)
    * SCRUM-40. Favorite managing in progress, nothing is tested yet.
    
    * SCRUM-40 addFavorite/getFavorites tested, Add to Favorites button implemented, not yet tested

--- Backend/services/tmdbService.js
+++ Backend/services/tmdbService.js
@@ -210,4 +210,31 @@ async function searchMoviesAndTvCriteria(query, page = 1) {
   };
 }
 
-export { getNowPlayingMovies, searchMoviesAndTv, searchMoviesAndTvCriteria };
+//This part is for favorite routes, to get the media by id and media type from TMDB API
+//This part is not tested yet, but it is used in the favorite routes to get the media by id and media type from TMDB API
+async function getMoviesById(tmdbId, mediaType) {
+  const url =
+    `${TMDB_BASE_URL}/${mediaType}/${tmdbId}?language=en-US`;
+
+  const response = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `TMDB API request failed with status ${response.status}`,
+    );
+  }
+
+  const data = await response.json();
+
+  return normalizeSearchResult({
+    ...data,
+    media_type: mediaType,
+  });
+}
+
+export { getNowPlayingMovies, searchMoviesAndTv, searchMoviesAndTvCriteria, getMoviesById };
